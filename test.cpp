93344eec1905f15965fbff6075ee37a4a7ccd24f
Keyvan Fatehi <keyvanfatehi@gmail.com>
2012-09-09 21:08:18 -0700

    better wording

--- test.cpp
+++ test.cpp
@@ -3,24 +3,22 @@
 
 using namespace std;
 
-void define_array(int arr[])
+void load_array(int arr[])
 {
   for(int i=0;i<5;i++)
     arr[i] = 10;
-  awefwaf
 }
 
 void print_array(int arr[])
 {
   for(int i=0;i<5;i++)
     cout << arr[i] << endl;
-  fewafwef
 }
 
 
 int main()
 {
   int myArray[5];
-  define_array(myArray);
+  load_array(myArray);
   print_array(myArray);
 }
